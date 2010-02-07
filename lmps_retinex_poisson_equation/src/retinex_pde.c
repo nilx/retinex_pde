5a02c20e01fbc6746bbe2b74787eaffe7b1ec44d
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2010-02-07 04:34:18 +0100

    retinex_pde: drop mw4*, cleanup retinex_pde.c

--- lmps_retinex_poisson_equation/src/retinex_pde.c
+++ lmps_retinex_poisson_equation/src/retinex_pde.c
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2009, 2010 IPOL Image Processing On Line http://www.ipol.im/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * @file lib/src/algo_retinex_pde.c
  *
  * @author Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
  *
- * @todo adapt nb threads to the number of CPU available (OpenMP?)
+ * @todo adapt nb threads to the number of CPU available
  * @todo only multithread large images
  */
 
@@ -12,8 +29,6 @@
 #include <float.h>
 
 #include <fftw3.h>
-
-#include "mw4.h"
 
 /* M_PI is a POSIX definition */
 #ifndef M_PI
@@ -36,181 +51,208 @@
  *     + (F_{i, j - 1} - F_{i, j})
  *     + (F_{i, j + 1} - F_{i, j}) \f$.
  * On the border, differences with "outside of the array" are 0.
- * If the absolute value of difference is < tmin, 0 is used instead;
+ * If the absolute value of difference is < t, 0 is used instead;
  *
  * @param data_out output array
  * @param data_in input array
  * @param nx, ny array size
- * @param tmin threshold
+ * @param t threshold
  *
  * @return data_out
- *
- * @todo unloop?
+5A *
+ * @todo full iteration and switch()
  * @todo try using blas?
  */
 static float *discrete_laplacian_threshold(float *data_out,
-                                               const float *data_in,
-                                               size_t nx, size_t ny,
-                                               float tmin)
+					   const float *data_in,
+					   size_t nx, size_t ny, float t)
 {
     int i, j;
-    float *out_ptr;
-    const float *in_ptr, *in_ptr_xm1, *in_ptr_xp1, *in_ptr_ym1, *in_ptr_yp1;
+    float *ptr_out;
     float diff;
+    /* pointers to the current and neighbour values */
+    const float *ptr_in, *ptr_in_xm1, *ptr_in_xp1, *ptr_in_ym1, *ptr_in_yp1;
 
-    /* check allocation */
+    /* sanity check */
     if (NULL == data_in || NULL == data_out)
-        MW4_FATAL(MW4_MSG_NULL_PTR);
+    {
+        fprintf(stderr, "a pointer is NULL and should not be so\n");
+        abort();
+    }
 
     /* pointers to the data and neighbour values */
-    in_ptr = data_in;
-    in_ptr_xm1 = data_in - 1;
-    in_ptr_xp1 = data_in + 1;
-    in_ptr_ym1 = data_in - nx;
-    in_ptr_yp1 = data_in + nx;
-    out_ptr = data_out;
+    /*
+     *                 y-1
+     *             x-1 ptr x+1
+     *                 y+1
+     *    <---------------------nx------->
+     */
+    ptr_in = data_in;
+    ptr_in_xm1 = data_in - 1;
+    ptr_in_xp1 = data_in + 1;
+    ptr_in_ym1 = data_in - nx;
+    ptr_in_yp1 = data_in + nx;
+    ptr_out = data_out;
     /* iterate on j, i, following the array order */
     for (j = 0; j < (int) ny; j++)
         for (i = 0; i < (int) nx; i++)
         {
-            *out_ptr = 0.;
+            *ptr_out = 0.;
             /* row differences */
             if (0 < i)
             {
-                diff = *(in_ptr_xm1) - *in_ptr;
-                if (fabs(diff) > tmin)
-                    *out_ptr += diff;
+                diff = *(ptr_in_xm1) - *ptr_in;
+                if (fabs(diff) > t)
+                    *ptr_out += diff;
             }
             if ((int) nx - 1 > i)
             {
-                diff = *(in_ptr_xp1) - *in_ptr;
-                if (fabs(diff) > tmin)
-                    *out_ptr += diff;
+                diff = *(ptr_in_xp1) - *ptr_in;
+                if (fabs(diff) > t)
+                    *ptr_out += diff;
             }
             /* column differences */
             if (0 < j)
             {
-                diff = *(in_ptr_ym1) - *in_ptr;
-                if (fabs(diff) > tmin)
-                    *out_ptr += diff;
+                diff = *(ptr_in_ym1) - *ptr_in;
+                if (fabs(diff) > t)
+                    *ptr_out += diff;
             }
             if ((int) ny - 1 > j)
             {
-                diff = *(in_ptr_yp1) - *in_ptr;
-                if (fabs(diff) > tmin)
-                    *out_ptr += diff;
+                diff = *(ptr_in_yp1) - *ptr_in;
+                if (fabs(diff) > t)
+                    *ptr_out += diff;
             }
-            in_ptr++;
-            in_ptr_xm1++;
-            in_ptr_xp1++;
-            in_ptr_ym1++;
-            in_ptr_yp1++;
-            out_ptr++;
+            ptr_in++;
+            ptr_in_xm1++;
+            ptr_in_xp1++;
+            ptr_in_ym1++;
+            ptr_in_yp1++;
+            ptr_out++;
         }
     return data_out;
 }
 
 /**
- * @brief scale the DFT parameters
+ * @brief perform a Poisson PDE in the Fourier DCT space
  *
  * @f$ u(i, j) = F(i, j) / (2 * (cos(PI * i / nx)
  *                               + cos(PI * j / ny)
- *                               - 2. - u / (nx * ny - 1))) @f$
+ *                               - 2. - 2. / (nx * ny - 1))) @f$
  *
- * @param data the dft complex coefficients, of size nx x ny
+ * The trigonometric data is only computed once if the function is
+ * called many times with the same nx, ny and m parameters (RGB planes).
+ *
+ * @param data the dct complex coefficients, of size nx x ny
  * @param nx, ny data array size
- * @param m global multiplication parameter (DFT normalization)
+ * @param m global multiplication parameter (DCT normalization)
  *
  * @return the data array, updated
+ *
+ * @todo handle nx=ny
  */
-static float *retinex_update_dft(float *data,
-                                     size_t nx, size_t ny, float m)
+static float *retinex_poisson_dct(float *data,
+				  size_t nx, size_t ny, float m)
 {
-    float *data_ptr, *data_ptr_end;
-    float *cosi_ptr, *cosi_ptr_end, *cosj_ptr, *cosj_ptr_end;
-    float *factor_ptr;
+    float *ptr_data, *ptr_end;
+    float *ptr_coef;
+    float *ptr_cosi, *ptr_cosi_end, *ptr_cosj, *ptr_cosj_end;
     int i, j;
     float cst;
-    float u = 2.;
     /* static data, kept for a potential re-use */
-    static float s_u = 0.;
+    /*
+     * we typically perform this on 3 RGB planes, and wand to keep the
+     * trigonometric data because its computation is expensive
+     */
     static float s_m = 0.;
     static size_t s_nx = 0, s_ny = 0;
-    static float *s_factor = NULL, *s_cosi = NULL, *s_cosj = NULL;
+    static float *s_coef = NULL, *s_cosi = NULL, *s_cosj = NULL;
 
-    MW4_DEBUGF("saved params: u=%g, m=%g, nx=%u, ny=%u",
-               s_u, s_m, (unsigned int) s_nx, (unsigned int) s_ny);
-    MW4_DEBUGF("new params  : u=%g, m=%g, nx=%u, ny=%u",
-               u, m, (unsigned int) nx, (unsigned int) ny);
-
-    /* if needed, recompute the factors */
+    /*
+     * if needed, recompute the coefs
+     * this computation is only needed if nx, ny and m are not the
+     * same as the previous call
+     */
     /*
      * fabs(u - s_u) is a trick to avoid float comparison,
      * with negligible cost
      */
-    if (nx != s_nx || ny != s_ny || 0. < fabs(u - s_u) || 0. < fabs(m - s_m))
+    if (nx != s_nx || ny != s_ny || 0. < fabs(m - s_m))
     {
-        /* (re) allocate the factors table */
-        if (NULL != s_factor)
-            free(s_factor);
-        if (NULL == (s_factor = (float *) malloc(sizeof(float) * nx * ny)))
-            MW4_FATAL(MW4_MSG_ALLOC_ERR);
-        /* allocate and fill the cosinus table arrays */
+        s_m = m;
+
         if (s_nx != nx)
         {
+	    /* (re) allocate the cosinus table */
             if (NULL != s_cosi)
                 free(s_cosi);
             if (NULL == (s_cosi = (float *) malloc(sizeof(float) * nx)))
-                MW4_FATAL(MW4_MSG_ALLOC_ERR);
-            cosi_ptr = s_cosi;
-            cosi_ptr_end = cosi_ptr + nx;
+	    {
+		fprintf(stderr, "allocation error\n");
+		abort();
+	    }
+	    /* fill the cosinus table */
+            ptr_cosi = s_cosi;
+            ptr_cosi_end = ptr_cosi + nx;
             i = 0;
-            while (cosi_ptr < cosi_ptr_end)
-                *cosi_ptr++ = 2. * cos((M_PI * i++) / nx);
+            while (ptr_cosi < ptr_cosi_end)
+                *ptr_cosi++ = 2. * cos((M_PI * i++) / nx);
         }
         s_nx = nx;
+
         if (s_ny != ny)
         {
+	    /* (re) allocate the sinus table */
             if (NULL != s_cosj)
                 free(s_cosj);
             if (NULL == (s_cosj = (float *) malloc(sizeof(float) * ny)))
-                MW4_FATAL(MW4_MSG_ALLOC_ERR);
-            cosj_ptr = s_cosj;
-            cosj_ptr_end = cosj_ptr + ny;
+	    {
+		fprintf(stderr, "allocation error\n");
+		abort();
+	    }
+	    /* fill the sinus table */
+            ptr_cosj = s_cosj;
+            ptr_cosj_end = ptr_cosj + ny;
             j = 0;
-            while (cosj_ptr < cosj_ptr_end)
-                *cosj_ptr++ = 2. * cos((M_PI * j++) / ny);
+            while (ptr_cosj < ptr_cosj_end)
+                *ptr_cosj++ = 2. * cos((M_PI * j++) / ny);
         }
         s_ny = ny;
 
-        /* (re)compute the factors */
-        s_u = u;
-        s_m = m;
-        cst = (2. * s_u) / (s_nx * s_ny - 1);
-        MW4_DEBUGF("DCT coefficients *="
-                   " %g / (cos(i * PI / %u) + cos(j * PI / %u) - 4. - %g)",
-                   s_m, (unsigned int) s_nx, (unsigned int) s_ny, cst);
-        factor_ptr = s_factor;
-        cosj_ptr = s_cosj;
-        cosj_ptr_end = s_cosj + ny;
-        cosi_ptr_end = s_cosi + nx;
-        while (cosj_ptr < cosj_ptr_end)
+        /* (re) allocate the coefs table */
+        if (NULL != s_coef)
+            free(s_coef);
+        if (NULL == (s_coef = (float *) malloc(sizeof(float) * nx * ny)))
+	{
+	    fprintf(stderr, "allocation error\n");
+		abort();
+	}
+
+        /* (re) compute the coefs */
+	/*
+	 * coef(i, j) = 1 / (cos(i) + cos(j) - 4 - 4 / (nx * ny - 1))
+ 	 */
+        cst = 4. / (s_nx * s_ny - 1);
+        ptr_coef = s_coef;
+        ptr_cosj = s_cosj;
+        ptr_cosj_end = s_cosj + s_ny;
+        ptr_cosi_end = s_cosi + s_nx;
+        while (ptr_cosj < ptr_cosj_end)
         {
-            cosi_ptr = s_cosi;
-            while (cosi_ptr < cosi_ptr_end)
-                *factor_ptr++ = s_m / (*cosi_ptr++ + *cosj_ptr - 4. - cst);
-            cosj_ptr++;
+            ptr_cosi = s_cosi;
+            while (ptr_cosi < ptr_cosi_end)
+                *ptr_coef++ = s_m / (*ptr_cosi++ + *ptr_cosj - 4. - cst);
+            ptr_cosj++;
         }
-        if (FLT_MIN > fabs(s_u))
-            *s_factor = 0.;
     }
 
-    data_ptr = data;
-    data_ptr_end = data_ptr + nx * ny;
-    factor_ptr = s_factor;
     /* scale the dct coefficients */
-    while (data_ptr < data_ptr_end)
-        *data_ptr++ *= *factor_ptr++;
+    ptr_data = data;
+    ptr_end = ptr_data + nx * ny;
+    ptr_coef = s_coef;
+    while (ptr_data < ptr_end)
+        *ptr_data++ *= *ptr_coef++;
 
     return data;
 }
@@ -223,13 +265,14 @@ static float *retinex_update_dft(float *data,
  * @brief retinex PDE implementation
  *
  * This function solves the Redinex PDE equation with forward and
- * backward DFT.
+ * backward DCT.
  *
  * The input array is processed as follow:
  *
  * @li a discrete laplacian is computed with a threshold;
  * @li this discrete laplacian array is symmetrised in both directions;
- * @li this data is transformed by forward DFT;
+ * @li this data is transformed by forward DFT (both steps can be
+ *     handled by a simple DCT);
  * @li the DFT data is modified by
  * @f$ \hat{u}(i, j) = \frac{\hat{F}(i, j)}
                             {2 \times (\cos(\frac{i \pi}{n_x})
@@ -238,18 +281,15 @@ static float *retinex_update_dft(float *data,
  * @li this data is transformed by backward DFT.
  *
  * @param data input/output array
- * @param nx dimension
- * @param ny dimension
+ * @param nx, ny dimension
  * @param t retinex threshold
  *
  * @return data, or NULL if an error occured
  *
- * @todo unroll loops
  * @todo inplace fft
  * @todo split pre/run/post for multi-threading
  */
-float *retinex_pde(float *data,
-                       size_t nx, size_t ny, float t)
+float *retinex_pde(float *data, size_t nx, size_t ny, float t)
 {
     fftwf_plan dct_fw, dct_bw;
     float *data_fft, *data_tmp;
@@ -260,63 +300,55 @@ float *retinex_pde(float *data,
 
     /* check allocaton */
     if (NULL == data)
-        MW4_FATAL(MW4_MSG_NULL_PTR);
+    {
+        fprintf(stderr, "a pointer is NULL and should not be so\n");
+        abort();
+    }
+
     /* start threaded fftw if FFTW_NTHREADS is defined */
 #ifdef FFTW_NTHREADS
     if (0 == fftwf_init_threads())
-        MW4_FATAL("fftw initialisation error");
+    {
+        fprintf(stderr, "fftw initialisation error\n");
+        abort();
+    }
     fftwf_plan_with_nthreads(FFTW_NTHREADS);
 #endif                          /* FFTW_NTHREADS */
-    /* allocate the float-complex FFT array */
-    if (NULL == (data_fft = (float *) malloc(sizeof(float) * nx * ny)))
-        MW4_FATAL(MW4_MSG_ALLOC_ERR);
-    /* allocate the float tmp array */
-    if (NULL == (data_tmp = (float *) malloc(sizeof(float) * nx * ny)))
-	MW4_FATAL(MW4_MSG_ALLOC_ERR);
+
+    /* allocate the float-complex FFT array and the float tmp array */
+    if (NULL == (data_fft = (float *) malloc(sizeof(float) * nx * ny))
+	|| NULL == (data_tmp = (float *) malloc(sizeof(float) * nx * ny)))
+    {
+	fprintf(stderr, "allocation error\n");
+	abort();
+    }
 
     /*
-     * step one : fill the DFT input with symmetrised discrete laplacian
+     * retinex PDE
      */
 
-    /* compute the laplacian and store it in data_tmp */
+    /* compute the laplacian : data -> data_tmp */
     (void) discrete_laplacian_threshold(data_tmp, data,
                                             nx, ny, t);
-
-    /*
-     * step two : DCT
-     */
-
-    /* create the DFT forward plan and run the DCT */
+    /* create the DFT forward plan and run the DCT : data_tmp -> data_fft */
     dct_fw = fftwf_plan_r2r_2d((int) ny, (int) nx,
                                data_tmp, data_fft,
                                FFTW_REDFT10, FFTW_REDFT10,
                                FFTW_ESTIMATE | FFTW_DESTROY_INPUT);
     fftwf_execute(dct_fw);
-
     free(data_tmp);
 
-    /*
-     * step three : update the DCT result
-     */
+    /* solve the Poisson PDE in Fourier space */
+    (void) retinex_poisson_dct(data_fft, nx, ny, 1. / (float) (nx * ny));
 
-    (void) retinex_update_dft(data_fft, nx, ny, 1. / (float) (nx * ny));
-
-    /*
-     * step four : backward DCT
-     */
-
-    /* create the DFT backward plan and run the DCT */
+    /* create the DFT forward plan and run the iDCT : data_fft -> data */
     dct_bw = fftwf_plan_r2r_2d((int) ny, (int) nx,
                                data_fft, data,
                                FFTW_REDFT01, FFTW_REDFT01,
                                FFTW_ESTIMATE | FFTW_DESTROY_INPUT);
     fftwf_execute(dct_bw);
 
-    /*
-     * cleanup
-     */
-
-    /* destroy the FFT plan and data */
+    /* cleanup */
     fftwf_destroy_plan(dct_fw);
     fftwf_destroy_plan(dct_bw);
     fftwf_free(data_fft);
