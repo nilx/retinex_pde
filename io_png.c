f13ee3cf3417845e317490b2af8229c6c35e58fa
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2010-08-11 18:28:45 +0900

    updated io_png

--- io_png.c
+++ io_png.c
@@ -2,33 +2,15 @@
  * Copyright (c) 2010, Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above
- *    copyright notice, this list of conditions and the following
- *    disclaimer in the documentation and/or other materials provided
- *    with the distribution.
+ * This program is free software: you can redistribute it and/or modify
+ * it under, at your option, the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version, or
+ * the terms of the simplified BSD license.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
- * THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * The views and conclusions contained in the software and
- * documentation are those of the authors and should not be
- * interpreted as representing official policies, either expressed
- * or implied, of the copyright holder.
+ * You should have received a copy of these licenses along this
+ * program. If not, see <http://www.gnu.org/licenses/> and
+ * <http://www.opensource.org/licenses/bsd-license.html>.
  */
 
 /**
@@ -53,6 +35,7 @@
  * @todo add a test suite
  * @todo internally handle RGB/gray conversion in read_png_raw()
  * @todo handle deinterlacing as a libpng transform function
+ * @todo openmp parallel loops
  *
  * @author Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
  */
@@ -63,7 +46,7 @@
 #include <assert.h>
 
 /* option to use a local version of the libpng */
-#ifdef _LOCAL_LIBS
+#ifdef WITH_LOCAL_LIBPNG
 #include "png.h"
 #else
 #include <png.h>
--- io_png.h
+++ io_png.h
@@ -2,7 +2,7 @@
 extern "C" {
 #endif
 
-#define IO_PNG_VERSION 0.20100727
+#define IO_PNG_VERSION 0.20100803
 
 /* io_png.c */
 unsigned char *read_png_u8(const char *fname, size_t *nx, size_t *ny, size_t *nc);
