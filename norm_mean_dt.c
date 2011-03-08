37dd7d432ac32c9a625306ee50a856f85935e7ba
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2011-03-08 15:30:53 +0900

    norm_mean_dt.c file header (licence and doc)

--- norm_mean_dt.c
+++ norm_mean_dt.c
@@ -1,31 +1,45 @@
+/*
+ * Copyright 2011 IPOL Image Processing On Line http://www.ipol.im/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file norm_brief_dt.c
+ * @brief image normalisation
+ *
+ * @author Jose-Luis Lisani <joseluis.lisani@uib.es>
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
-
 /**
-* @brief normalize mean and variance of an image given a reference image
-*
-* @param out input/output data
-* @param in reference image 
-* @param size size of the input/output
-*
-* 
-*/
-
-
-
-
-
+ * @brief normalize mean and variance of an image given a reference
+ *        image
+ *
+ * @param out input/output data
+ * @param in reference image 
+ * @param size size of the input/output
+ */
 void norm_dt( float *out, float *in, size_t size )
-
 {
- 
       float m_out,dt_out,a,b, m_in, dt_in, x;
       float *ptr_in, *ptr_out,*ptr_end;
      
-
       /* sanity check*/
        if (NULL == out || NULL == in)
        {
