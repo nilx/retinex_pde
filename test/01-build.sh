6a8bf5963f1ac13fb619442fe05b796d9c71ff66
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2011-06-22 15:02:30 +0900

    improve build test
    * add nwcc compiler
    * support per-compilr options

--- test/01-build.sh
+++ test/01-build.sh
@@ -23,21 +23,17 @@ _log make clean
 _log make
 
 echo "* compiler support"
-for CC in cc c++ gcc g++ tcc clang icc pathcc; do
-    if which $CC; then
-	echo "* $CC compiler"
-	_log make distclean
-	_log make CC=$CC CFLAGS=
-	_log _test_run
-    fi
-done
-for CC in gcc g++ clang; do
-    if which $CC; then
-	echo "* $CC compiler with flags"
-	_log make distclean
-	_log make CC=$CC
-	_log _test_run
-    fi
+for CC in cc c++ gcc g++ tcc nwcc clang icc pathcc; do
+    which $CC || continue
+    echo "* $CC compiler"
+    _log make distclean
+    case $CC in
+	"gcc"|"g++")
+	    _log make CC=$CC ;;
+	*)
+	    _log make CC=$CC CFLAGS= ;;
+    esac
+    _log _test_run
 done
 
 _log make distclean
