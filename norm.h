e049e3d42981b6b33e36dac837bf3a8d0613c9a2
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2012-10-09 13:01:20 +0900

    C++-enabled and multi-inclusion-safe headers

--- norm.h
+++ norm.h
@@ -1,2 +1,15 @@
+#ifndef _NORM_H
+#define _NORM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* norm.c */
 void normalize_mean_dt(float *data, const float *ref, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !_NORM_H */
--- retinex_pde_lib.h
+++ retinex_pde_lib.h
@@ -1,2 +1,15 @@
+#ifndef _RETINEX_PDE_LIB_H
+#define _RETINEX_PDE_LIB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* retinex_pde_lib.c */
 float *retinex_pde(float *data, size_t nx, size_t ny, float t);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !_RETINEX_PDE_LIB_H */
